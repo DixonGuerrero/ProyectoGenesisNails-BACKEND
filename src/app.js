1d85282f24aef69a0728772904f1c702c5d2b048
Proyecto Madagascar Films <145238589+MadagascarFilms@users.noreply.github.com>
2024-03-13 12:22:26 -0500

    cors

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 //import usuarioRoutes from "./routes/usuario.routes.js";
 import personaRoutes from "./routes/persona.routes.js";
 import proveedorRoutes from "./routes/proveedor.routes.js";
@@ -16,8 +17,10 @@ import authRoutes from "./routes/auth.routes.js";
 
 import salidaRoutes from "./routes/salida.routes.js";
 
+
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 //app.use("/api/", usuarioRoutes);
 app.use("/api/persona/", personaRoutes);
@@ -39,5 +42,11 @@ app.use((req, res) => {
     message: "Ruta no encontrada",
   });
 });
+
+
+
+
+
+
 
 export default app;
