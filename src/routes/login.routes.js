02fbdadf37fc4b185912538ac3cab9dfbfa98074
Proyecto Madagascar Films <145238589+MadagascarFilms@users.noreply.github.com>
2024-03-14 15:31:54 -0500

    Update routes register and login

--- src/routes/login.routes.js
+++ src/routes/login.routes.js
@@ -1,11 +1,11 @@
 import { Router } from "express";
-import { login} from "../controllers/auth.controller.js";
+import { login } from "../controllers/auth.controller.js";
 import {
   validacionLogin,
   validacionPersona,
 } from "../middlewares/middleware.js";
 const router = Router();
 
-router.post("/login", validacionLogin, login);
+router.post("/", validacionLogin, login);
 
 export default router;
