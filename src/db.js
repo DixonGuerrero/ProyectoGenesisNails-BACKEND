3564c6b179876af452552025499bd70b464c0203
Proyecto Madagascar Films <145238589+MadagascarFilms@users.noreply.github.com>
2024-03-22 15:41:33 -0500

    Actualizacion Base de datos

--- src/db.js
+++ src/db.js
@@ -1,4 +1,8 @@
 import { createPool } from "mysql2/promise";
+import fs from "fs";
+import { fileURLToPath } from 'url';
+import path from "path";
+
 import {
   PORT,
   DB_HOST,
@@ -8,10 +12,19 @@ import {
   DB_PORT,
 } from "./config.js";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const sslCAPath = path.join(__dirname, 'private', 'DigiCertGlobalRootCA.crt.pem');
+const sslCA = fs.readFileSync(sslCAPath, 'utf8');
 export const pool = createPool({
   host: DB_HOST,
   user: DB_USER,
   password: DB_PASSWORD,
   port: DB_PORT,
   database: DB_NAME,
+  ssl: {
+    ca: sslCA,
+  },
+  timezone: 'America/Bogota',
 });
